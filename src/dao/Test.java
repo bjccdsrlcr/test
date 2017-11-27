0b7c129e9c3bf4a84978ee674ba3ed2afe46fe38
bjccdsrlcr <1015360603@qq.com>
2017-11-27 14:41:06 +0800

    com.mysql.jdbc.Driver在属性文件中不需要再加双引号

--- src/dao/Test.java
+++ src/dao/Test.java
@@ -1,5 +1,6 @@
 package dao;
 
+
 /**
  * Author: bjccdsrlcr
  * Date: 2017/11/23 0023
@@ -9,5 +10,6 @@ public class Test {
     public static void main(String[] args) {
         ForumDao forumDao = new ForumDao();
         forumDao.initDao();
+
     }
 }
